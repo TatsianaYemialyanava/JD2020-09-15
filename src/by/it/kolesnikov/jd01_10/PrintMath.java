35b0a3e99c5d2120436d161165232a38aab041ac
Ghostik13 <roxan95@,ail.ru>
2020-10-06 19:43:10 +0300

    jd01_10 TaskA and TaskB

--- src/by/it/kolesnikov/jd01_10/PrintMath.java
+++ src/by/it/kolesnikov/jd01_10/PrintMath.java
@@ -1,5 +1,6 @@
 package by.it.kolesnikov.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
@@ -15,12 +16,29 @@ public class PrintMath {
                 if(Modifier.isStatic(modifiers))
                     out.append("static ");
                 Class<?> returnType = method.getReturnType();
+                out.append(returnType.getSimpleName()).append(' ');
                 out.append(method.getName()).append("(");
                 Class<?>[] types = method.getParameterTypes();
                 String delimeter ="";
                 for (Class<?> type:types){
-                    out.append(delimeter);
+                    out.append(delimeter).append(type.getTypeName());
+                    delimeter=",";
                 }
+                out.append(")");
+                System.out.println(out);
+            }
+        }
+        Field [] fields = structMath.getDeclaredFields();
+        for (Field field: fields) {
+            int modifiers = field.getModifiers();
+            if (Modifier.isPublic(modifiers)) {
+                StringBuilder out = new StringBuilder();
+                out.append("public ");
+                if(Modifier.isStatic(modifiers))
+                    out.append("static ");
+                Class<?> returnType = field.getType();
+                out.append(returnType.getSimpleName()).append(' ');
+                out.append(field.getName());
                 System.out.println(out);
             }
         }
