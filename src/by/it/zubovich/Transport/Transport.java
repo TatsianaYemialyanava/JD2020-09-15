bc8ba140edc28de0eb32b63fabaac805f77ac2b4
maer1 <tutbora@gmail.com>
2020-10-08 11:26:25 +0300

    moved last files from old jd01_07 wisl settings for lambdas

---
+++ src/by/it/zubovich/Transport/Transport.java
@@ -0,0 +1,26 @@
+package by.it.zubovich.Transport;
+
+public abstract class Transport implements Comparable {
+    private double speed;
+    private int passenger;
+
+
+    Transport(double speed, int passenger){
+        this.speed = speed;
+        this.passenger = passenger;
+
+
+    }
+
+    public void print() {
+        System.out.println("Это объект транспорт");
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public int getPassenger() {
+        return passenger;
+    }
+}
