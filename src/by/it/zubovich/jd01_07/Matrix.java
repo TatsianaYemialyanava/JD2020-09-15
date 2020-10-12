bf8ed692535b6ff62afe89b661176f80f29cbea5
maer1 <tutbora@gmail.com>
2020-10-13 00:20:38 +0300

    jd2020-09-15 before saving

--- src/by/it/zubovich/jd01_07/Matrix.java
+++ src/by/it/zubovich/jd01_07/Matrix.java
@@ -7,34 +7,41 @@ public class Matrix extends Var {
 
     Matrix(double[][] value) {
         this.value = value;
-
     }
 
     Matrix(Matrix matrix) {
+
         this.value = matrix.value;
-
-
     }
 
     Matrix(String strMatrix) {
-        String[] result = strMatrix.split("}, ");
+
+
+/*        String[] result = strMatrix.split("}, ");
 
         for (int i = 0; i < result.length; i++) {
             String[] resultColumn = result[i].replace("{", "").replace("}","").trim().split(",");
             this.value = new double[result.length][resultColumn.length];
-            for (int j = 0; j < value[i].length; j++) {
+            for (int j = 0; j < resultColumn.length; j++) {
                 this.value[i][j] = Double.parseDouble(resultColumn[j]);
 
             }
-            System.out.println();
+        }*/
+        String[] result = strMatrix.split(",");
+        this.value = new double[result.length][];
+        for (int i=0; i<result.length;i++) {
+            for(int j=0; j < result.length;j++) {
+
+
+            }
         }
+        System.out.println(Arrays.deepToString(result).replace("[","").replace("]",""));
 
-    }
-
+            }
     @Override
     public String toString() {
-
         return (Arrays.deepToString(value).replace("[", "{").replace("]", "}"));
-    }
 
+
+    }
 }
--- src/by/it/zubovich/jd01_07/Runner.java
+++ src/by/it/zubovich/jd01_07/Runner.java
@@ -2,14 +2,29 @@ package by.it.zubovich.jd01_07;
 
 public class Runner {
     public static void main(String[] args) {
-        Scalar n = new Scalar("3.1415");
-        System.out.println(n.toString());
-        Scalar m = new Scalar(3.1415);
-        System.out.println(m.toString());
-        Scalar h = new Scalar(m);
-        System.out.println(h.toString());
-        double [][] mas = {{1.0,2.0},{3.0,4.0}};
-        Matrix f = new Matrix(mas);
-        System.out.println(f.toString());
+        Scalar sNum = new Scalar(3.1415);
+        System.out.println("\n" + sNum.toString());
+        Scalar sVar = new Scalar(sNum);
+        System.out.println(sVar.toString());
+        Scalar sStr = new Scalar("3.1415");
+        System.out.println(sStr.toString() + "\n");
+
+        Vector vArr = new Vector(new double[]{1.0, 2.0, 4.0});
+        System.out.println(vArr.toString());
+        Vector vVar = new Vector(vArr);
+        System.out.println(vVar.toString());
+        Vector vStr = new Vector("{1.0, 2.0, 4.0}");
+        System.out.println(vStr.toString() + "\n");
+
+        Matrix mArr = new Matrix(new double[][]{
+                {1.0,2.0},
+                {3.0,4.0}
+        });
+        System.out.println(mArr.toString());
+        Matrix mVar = new Matrix(mArr);
+        System.out.println(mVar.toString());
+        Matrix mStr = new Matrix("{{ 1.0, 2.0 },{ 3.0, 4.0 }}");
+        System.out.println(mStr.toString());
+
     }
 }
