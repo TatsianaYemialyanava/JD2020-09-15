9f5b3b1d860192919631a988dd2dd20ca81db595
jmabracodabra <jmabracodabra@gmail.com>
2020-10-14 15:51:36 +0300

    Implemented jd01_13 TaskA, TaskB, TaskC

--- src/by/it/frolova/jd01_13/TaskC.java
+++ src/by/it/frolova/jd01_13/TaskC.java
@@ -10,5 +10,42 @@ package by.it.frolova.jd01_13;
  Числа нужно выводить без форматирования, через пробел, в порядке обратном вводу.
  После 5 допущенных ошибок программа должна завершиться, пробрасывая ошибку в JVM.*/
 
+import java.util.*;
+
 public class TaskC {
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        readData();
+    }
+
+    static void readData() {
+        List<Double> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            try {
+                while (true) {
+                    double input = Double.parseDouble(scanner.next());
+                    list.add(input);
+                }
+            } catch (NumberFormatException e) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                printValues(list);
+            }
+        }
+    }
+
+    private static void printValues(List<Double> list) {
+        for (int j = list.size() - 1; j >= 0; j--) {
+            if (j > 0) {
+                System.out.print(list.get(j) + " ");
+            } else {
+                System.out.println(list.get(j));
+            }
+        }
+    }
 }
