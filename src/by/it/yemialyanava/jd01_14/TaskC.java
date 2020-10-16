4702ae8a02b0cd4031e57e2175814f95d443611e
TatsianaYemialyanava <tatsiana.yemialyanava@gmail.com>
2020-10-16 13:17:51 +0300

    jd01_14 files add

--- src/by/it/yemialyanava/jd01_14/TaskC.java
+++ src/by/it/yemialyanava/jd01_14/TaskC.java
@@ -5,13 +5,16 @@ package by.it.yemialyanava.jd01_14;
          Продублировать вывод в консоль в файл resultTaskC.txt*/
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Objects;
 
 public class TaskC {
 
     private static final String TEXT_DATA = "text.txt";
     private static final String SRC = "src";
     private static final String USER_DIR = "user.dir";
-    private static final String RESULT_TASK_С = "resultTaskС.txt";
+    private static final String RESULT_TASK_C = "resultTaskС.txt";
 
     private static String getPath(Class<?> aClass) {
         String packageName = aClass
@@ -23,20 +26,43 @@ public class TaskC {
         return root + File.separator + SRC + File.separator + packageName;
     }
     public static void main(String[] args) {
-        String textName = getPath(TaskC.class) + TEXT_DATA;
+        String pathToDir14 = getPath(TaskC.class);
+        File file14 = new File(pathToDir14);
+        StringBuilder output = new StringBuilder();
+        if (file14.isDirectory()){
+            File packageName = file14.getParentFile();
+            File [] arrayFiles = packageName.listFiles();
+            for (File element: arrayFiles) {
+                if(element.isFile()){
+                    output.append("file:" + element.getName()).append("\n");
+                    System.out.println("file:" + element.getName());
+                }else if (element.isDirectory()){
+                    output.append("dir:" + element.getName()).append("\n");
+                    System.out.println("dir:" + element.getName());
+                    File packageName = file14.getParentFile();
+                    File [] arrayFiles = packageName.listFiles();
 
-
-
-        /*public static void main(String a[]){
-            File file = new File("C:/MyFolder/");
-            String[] fileList = file.list();
-            for(String name:fileList){
-                System.out.println(name);
+                }
             }
-        }*/
+        }
+        String fileNameTxt = getPath(TaskC.class) + RESULT_TASK_C;
+        printToFile(output.toString(), fileNameTxt);
 
 
+    }
+    private static void printToFile(String strToFile, String fileNameTxt) {
+        PrintWriter printWriter = null;
+        try {
+            printWriter = new PrintWriter(fileNameTxt);
+            printWriter.println(strToFile);
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (Objects.nonNull(printWriter)) {
+                printWriter.close();
+            }
+        }
     }
 
 }
