aeb0460a3666eee75d14d55ffb1726a0c9967aeb
Ghostik13 <roxan95@,ail.ru>
2020-10-16 21:41:52 +0300

    jd014 A

---
+++ src/by/it/kolesnikov/jd01_14/TaskA.java
@@ -0,0 +1,73 @@
+package by.it.kolesnikov.jd01_14;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+class TaskA {
+    //Объявляем контейнеры с константами (удобнее)
+    static final String FILE_DATA = "dataTaskA.bin";
+    static final String SRC = "src";
+    static final String USER_DIR = "user.dir";
+    public static final String RESULT_TASK_A = "resultTaskA.txt";
+
+    static String getPath(Class<TaskA> aClass) {
+        String packageName = aClass
+                .getPackage()
+                .getName()
+                .replace(".", File.separator)
+                .concat(File.separator);
+        //получаем корень проекта Root
+        String property = System.getProperty(USER_DIR);
+        return property + File.separator + SRC + File.separator + packageName;
+    }
+
+    public static void main(String[] args) {
+        String filename = getPath(TaskA.class) + FILE_DATA;
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(filename);
+                BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+                DataOutputStream dataOutputStream = new DataOutputStream(bufferedOutputStream)) {
+            for (int i = 0; i < 20; i++) {
+                int n = -12345 / 2 + (int) (Math.random() * 12345);
+                dataOutputStream.writeInt(n);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<Integer> list = new ArrayList<>();
+        try (DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)))) {
+            while (dataInputStream.available() > 0) {
+                list.add(dataInputStream.readInt());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //print to console
+        double sum = 0;
+        for (Integer integer : list) {
+            System.out.println(integer + " ");
+            sum += integer;
+        }
+        System.out.println("\navg=" + sum / list.size());
+        String filenametxt = getPath(TaskA.class)+ RESULT_TASK_A;
+        //print to file
+        PrintWriter printWriter = null;
+        try {
+            printWriter=new PrintWriter(filenametxt);
+            double sum2 = 0;
+            for (Integer integer : list) {
+                System.out.println(integer + " ");
+                sum += integer;
+            }
+            System.out.println("\navg=" + sum2 / list.size());
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }finally {
+            if (Objects.nonNull(printWriter)){
+
+            }
+        }
+    }
+}
