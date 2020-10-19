29b109a2eb02703ebeefa9ac19297ac768e3c542
MoiseyenkaArtsiom <5700753@gmail.com>
2020-10-19 23:11:18 +0300

    01_15A

--- src/by/it/moiseyenko/jd01_13/TaskA.java
+++ src/by/it/moiseyenko/jd01_13/TaskA.java
@@ -4,10 +4,25 @@ import java.util.HashMap;
 
 public class TaskA {
     public static void main(String[] args) {
-        if (Math.random()>0.5)
-            new HashMap<String, String>(null);
-        else
-            Integer.parseInt("привет");
-
+        try {
+            if (Math.random() > 0.5)
+                new HashMap<String, String>(null);
+            else
+                Integer.parseInt("привет");
+        } catch (NumberFormatException | NullPointerException e) {
+            Class<? extends RuntimeException> aClass = e.getClass();
+            String exceptionName = aClass.getName();
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            for (StackTraceElement traceElement : stackTrace) {
+                String methodName = traceElement.getMethodName();
+                if (methodName.equals("main")) {
+                    String className = traceElement.getClassName();
+                    int lineNumber = traceElement.getLineNumber();
+                    System.out.printf("  name: %s\n class: %s\n  line:%d\n",
+                            exceptionName, className, lineNumber);
+                    break;
+                }
+            }
+        }
     }
 }
