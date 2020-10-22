bd186857c1434d49802cab1bd2ff9b9ac01823bd
Olga <https://github.com/OlgaDobrodey/JD2020-09-15>
2020-10-22 20:09:08 +0300

    jd02_01 work (basket)

--- src/by/it/dobrodey/jd02_01/Basket.java
+++ src/by/it/dobrodey/jd02_01/Basket.java
@@ -10,7 +10,6 @@ public class Basket extends Thread implements IUseBasket {
     }
 
 
-
     @Override
     public void takeBasket() {
         System.out.println(this + "          " + "take basket");
