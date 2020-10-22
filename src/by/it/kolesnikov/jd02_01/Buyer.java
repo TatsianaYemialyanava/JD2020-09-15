299d3219bfd6a146e8becc7e2b90dc6bbc49b479
Ghostik13 <roxan95@,ail.ru>
2020-10-22 08:45:40 +0300

    JD02_01 A, B

---
+++ src/by/it/kolesnikov/jd02_01/Buyer.java
@@ -0,0 +1,53 @@
+package by.it.kolesnikov.jd02_01;
+
+class Buyer extends Thread implements IBuyer, IUseBasket {
+
+    public Buyer(int number){
+        this.setName("Buyer №"+number);
+    }
+
+    @Override
+    public void run() {
+        enterToMarket();
+        takeBasket();
+        choseGoods();
+        putGoodsToBasket();
+        goOut();
+    }
+
+    @Override
+    public void takeBasket(){
+
+        System.out.println(this+" took basket");
+    }
+
+    @Override
+    public void putGoodsToBasket(){
+        System.out.print(this+" put next goods into the basket: ");
+        Basket.basket();
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
+    @Override
+    public void enterToMarket() {
+        System.out.println(this+" entered to Market");
+    }
+
+    @Override
+    public void choseGoods() {
+        System.out.println(this+" started to choose goods");
+        int timeout=Helper.getRandom(500, 2000);
+        Helper.timeOut(timeout);
+        System.out.println(this+" finished to choose goods");
+    }
+
+    @Override
+    public void goOut() {
+        System.out.println(this+" went out of Market");
+    }
+
+}
---
+++ src/by/it/kolesnikov/jd02_01/IBuyer.java
@@ -0,0 +1,7 @@
+package by.it.kolesnikov.jd02_01;
+
+interface IBuyer {
+    void enterToMarket();
+    void choseGoods();
+    void goOut();
+}
