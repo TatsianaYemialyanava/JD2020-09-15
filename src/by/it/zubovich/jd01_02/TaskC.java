0f97f646019ba60de2b8b3ee715e183fbcffb5f9
maer1 <tutbora@gmail.com>
2020-10-24 20:28:45 +0300

    jd2020-09-15 before saving I finished 12A 13A 14A To be continue...

--- src/by/it/zubovich/jd01_02/TaskC.java
+++ src/by/it/zubovich/jd01_02/TaskC.java
@@ -1,37 +1,44 @@
 package by.it.zubovich.jd01_02;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class TaskC {
     public static void main(String[] args) {
         System.out.println("введите натуральное число, или не вводите ничего:");
         Scanner sc = new Scanner(System.in);
-        int[][] n = step1(sc.nextInt());
-        System.out.println(Arrays.deepToString(n));
-        step2(n);
-        step3(n);
+        int[][] arr = step1(sc.nextInt());
+        System.out.println(Arrays.deepToString(arr));
+        step2(arr);
+        step3(arr);
 
     }
-    static int[][] step1(int n) {
+    private static int[][] step1(int n) {
         int[][] arr = new int[n][n];
-        boolean minOk;
         boolean maxOk;
+        boolean minOk;
         do {
             maxOk = false;
             minOk = false;
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr[i].length; j++) {
-                    arr[i][j] = -5 + (int) (Math.random() * (10 + 1));
-                    if (arr[i][j] == 5) maxOk = true;
-                    if (arr[i][j] == -5) minOk = true;
+            for (int i = 0; i < arr.length; ++i) {
+                for (int j = 0; j < arr[i].length; ++j) {
+                    int min = -n;
+                    int diff = n - min;
+                    Random random = new Random();
+                    int r = random.nextInt(diff + 1);
+                    r += min;
+                    arr[i][j] = r;
+                    if (arr[i][j] == n) maxOk = true;
+                    if (arr[i][j] == -n) minOk = true;
                 }
             }
         }
         while (!minOk || !maxOk);
         return arr;
     }
-    static int step2(int[ ][ ] arr) {
+
+    private static int step2(int[ ][ ] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             int temp = 0;
@@ -47,7 +54,7 @@ public class TaskC {
         System.out.println("\n" + sum);
         return sum;
     }
-    static int[ ][ ] step3(int[ ][ ] arr){
+    private static int[ ][ ] step3(int[ ][ ] arr){
 
 
         return arr;
