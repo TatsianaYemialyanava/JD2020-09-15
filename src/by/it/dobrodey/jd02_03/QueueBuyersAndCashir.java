872181b165f3d914c10098eb532907acdeaf6eb7
Olga <https://github.com/OlgaDobrodey/JD2020-09-15>
2020-10-25 21:01:45 +0300

    jd02_03 A,B,C Finish

---
+++ src/by/it/dobrodey/jd02_03/QueueBuyersAndCashir.java
@@ -0,0 +1,66 @@
+package by.it.dobrodey.jd02_03;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class QueueBuyersAndCashir {
+
+    private static final Object monitor = new Object();
+    private static final Object monCashier = new Object();
+    private static BlockingDeque<Buyer> deque = new LinkedBlockingDeque<>(30);
+    private static BlockingDeque<Buyer> dequepensionner = new LinkedBlockingDeque<>(30);
+    private static BlockingDeque<Cashier> dequeCashier = new LinkedBlockingDeque<>(5);
+
+    static int getSizeCashier() {
+        synchronized (monCashier) {
+            return dequeCashier.size();
+        }
+    }
+
+    static int getSize() {
+        return deque.size() + dequepensionner.size();
+    }
+    static int getSizePensionner() {
+        return dequepensionner.size();
+    }
+
+    static void add(Buyer buyer) {
+        {
+            if (buyer.toString().contains("pensioneer")) {
+                try {
+                    dequepensionner.putLast(buyer);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                try {
+                    deque.putLast(buyer);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    static Buyer extract() {
+        if(getSizePensionner()!=0){
+            System.out.println("QueuePensionner:");
+            return dequepensionner.pollFirst();
+        }
+        else {return deque.pollFirst();}
+    }
+
+
+
+    static void closeCashier(Cashier cashier) {
+        dequeCashier.addFirst(cashier);
+        System.out.printf("%s closed\n", cashier);
+    }
+
+    static Cashier openCashier() {
+        return dequeCashier.pollFirst();
+    }
+}
+
