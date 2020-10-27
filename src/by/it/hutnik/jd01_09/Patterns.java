c27c5a6cc6a4289e3510bf117ee25ff67f46f79e
Daddy <matrimaxim@gmail.com>
2020-10-27 11:44:22 +0300

    Done more tasks...

--- src/by/it/hutnik/jd01_09/Patterns.java
+++ src/by/it/hutnik/jd01_09/Patterns.java
@@ -2,7 +2,6 @@ package by.it.hutnik.jd01_09;
 
 interface Patterns {
     String OPERATION="[-+/*]";
-    String SCALAR="\\p";
-//    String SCALAR="-?[0-9]+(\\.[0-9]+)?";
+    String SCALAR="-?[0-9]+(\\.[0-9]+)?";
     String VECTOR="\\{"+SCALAR+"(\\,\\s?"+SCALAR+")*}";
 }
