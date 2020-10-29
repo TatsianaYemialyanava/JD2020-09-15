44586501e5e6696dd331ded21f1f98d3a83c9fc2
Alexander Khmelov <akhmelev@gmail.com>
2020-10-29 14:09:36 +0300

    jd02_05 classwork

---
+++ src/by/it/akhmelev/jd02_05/txt/convertor.bat
@@ -0,0 +1,4 @@
+native2ascii -encoding utf-8 keywords.txt ../resources/keywords.properties
+native2ascii -encoding utf-8 keywords_ru_RU.txt ../resources/keywords_ru_RU.properties
+native2ascii -encoding utf-8 keywords_be_BY.txt ../resources/keywords_be_BY.properties
+native2ascii -encoding utf-8 keywords_en_UK.txt ../resources/keywords_en_UK.properties
