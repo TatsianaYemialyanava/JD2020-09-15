c35d9b004ceeca492738b0a9f2f6391636c3839b
DevForEnjoy <49254510+DevForEnjoy@users.noreply.github.com>
2020-10-29 04:20:54 +0300

    done Jd02_04;

--- src/by/it/lapkovskiy/calculator/ParserTest.java
+++ src/by/it/lapkovskiy/calculator/ParserTest.java
@@ -16,7 +16,7 @@ public class ParserTest {
     }
 
     @Test
-    public void TaskA() throws CalcException {
+    public void TaskA_Scalar() throws CalcException {
         writeInParse("A=2+5.3",new Scalar(7.3));
 
         writeInParse("B=A*3.5",new Scalar(25.55));
@@ -25,11 +25,10 @@ public class ParserTest {
 
         writeInParse("B2=A/2-1",new Scalar(2.65));
     }
-
-
-
     @Test
-    public void TaskB() throws CalcException {
+    public void TaskB_Vector() throws CalcException {
+        parser.calc("A=2+5.3");
+        parser.calc("B=A*3.5");
         writeInParse("C=B+(A*2)",new Scalar(40.15));
 
         writeInParse("D=((C-0.15)-20)/(7-5)",new Scalar(10.0));
@@ -37,7 +36,7 @@ public class ParserTest {
         writeInParse("E={2,3}*(D/2)",new Vector("{10,15}"));
     }
     @Test
-    public void TaskC1() throws CalcException {
+    public void TaskC1_Matrix() throws CalcException {
         writeInParse("F={{1,2.0},{3,4}}+5",new Matrix("{{6.0,7.0},{8.0,9.0}}"));
         writeInParse("F={{1,2.0},{3,4}}+F",new Matrix("{{7.0,9.0},{11.0,13.0}}"));
 
@@ -48,6 +47,16 @@ public class ParserTest {
         writeInParse("F={{1,2.0},{3,4}}*F",new Matrix("{{35.0,50.0},{75.0,110.0}}"));
         writeInParse("F={{1,2.0},{3,4}}*{1,2}",new Vector("{5.0,11.0}"));
 
+    }
+    @Test
+    public void TaskC2_toString() throws CalcException {
+         Scalar scalar = new Scalar(1);
+         Vector vector = new Vector("{1,2.0}");
+         Matrix matrix = new Matrix("{{1,2.0},{3,4}}");
+
+        assertEquals("Error Scalar", "1.0", scalar.toString());
+        assertEquals("Error Vector", "{1.0,2.0}", vector.toString());
+        assertEquals("Error Matrix", "{{1.0,2.0},{3.0,4.0}}", matrix.toString());
     }
 
     public void writeInParse(String line,Var expected) throws CalcException {
